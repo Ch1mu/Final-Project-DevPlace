80cf4b1ad456f489822f2a2b73b36365fc5b5b6a
Ch1mu <frannluza@gmail.com>
2022-05-16 18:41:18 -0300

    clases user

--- ApiMessageSystem/src/main/java/FinalProject/ApiMessageSystem/Models/UserPerson.java
+++ ApiMessageSystem/src/main/java/FinalProject/ApiMessageSystem/Models/UserPerson.java
@@ -1,4 +1,26 @@
 package FinalProject.ApiMessageSystem.Models;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
 public class UserPerson {
+
+    @Id
+    private String username;
+    private String password;
+
+    @ManyToOne()
+    @JoinColumn(name = "role_id")
+    private Role role;
+
 }
