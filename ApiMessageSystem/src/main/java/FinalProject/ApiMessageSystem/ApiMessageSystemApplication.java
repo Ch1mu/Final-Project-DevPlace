3fbee3ed4df06c1acb814b966485d8c51280dafd
Ch1mu <frannluza@gmail.com>
2022-05-16 18:24:47 -0300

    Skeleton

---
+++ ApiMessageSystem/src/main/java/FinalProject/ApiMessageSystem/ApiMessageSystemApplication.java
@@ -0,0 +1,13 @@
+package FinalProject.ApiMessageSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiMessageSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiMessageSystemApplication.class, args);
+	}
+
+}
---
+++ ApiMessageSystem/src/test/java/FinalProject/ApiMessageSystem/ApiMessageSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package FinalProject.ApiMessageSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiMessageSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
