d169b28c7465e8bf6e5af9b3769c591a8ff4b25c
Ch1mu <frannluza@gmail.com>
2022-05-17 12:24:34 -0300

    chat repo con query

--- ApiMessageSystem/src/main/java/FinalProject/ApiMessageSystem/Repositories/ChatRepository.java
+++ ApiMessageSystem/src/main/java/FinalProject/ApiMessageSystem/Repositories/ChatRepository.java
@@ -2,8 +2,14 @@ package FinalProject.ApiMessageSystem.Repositories;
 
 import FinalProject.ApiMessageSystem.Models.Chat;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface ChatRepository extends JpaRepository<Chat, Long> {
 
     public Chat getById(long id);
+    @Query(value = "SELECT c.id_chat FROM chat c inner join person_per_chat ppc on ppc.id_chat = c.id_chat where ppc.username = :username GROUP BY c.id_chat", nativeQuery = true)
+    public List<Chat> getChatPerUser(@Param("username") String username);
 }
